739488e7e8ed18bf70e6f9c287a5cd8257149ad6
Miguel Angel Alvarez <malvarez@desarrolloweb.com>
2017-10-30 09:44:36 +0100

    Experimentar con los métodos de registro y login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  usuario = {
+    email: 'a@a.es',
+    password: '123'
+  };
+  registro = {
+    email: 'a@a.com',
+    password: 'qwe'
+  };
+
+  constructor (private http: Http) {}
+
+  ngOnInit() {}
+
+  onLogin() {
+    console.log(this.usuario);
+    this.http.post('http://localhost:3000/login', this.usuario)
+      .subscribe(
+        (response) => console.log(response)
+      );
+  }
+
+  onRegistro() {
+    this.http.post('http://localhost:3000/sign-in', this.registro)
+    .subscribe(
+      (response) => console.log(response),
+      (error) => console.log(error)
+    );
+  }
 }
